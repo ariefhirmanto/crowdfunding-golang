71751cf55711b702e1bd6c12425b97e3ee925868
ariefhirmanto <ariefhirmanto7@gmail.com>
2021-08-04 15:08:35 +0700

    initialize repo

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"startup/handler"
+	"startup/user"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3307)/startup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	UserRepository := user.NewRepository(db)
+	// service
+	UserService := user.NewService(UserRepository)
+	// handler
+	userHandler := handler.NewUserHandler(UserService)
+	// router
+	router := gin.Default()
+	api := router.Group("api/v1")
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run(":3000")
+	// fmt.Println("Connection to database succeed")
+}
