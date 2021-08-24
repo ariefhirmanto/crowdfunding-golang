fefb13290cd2c3f570bd68fc62da5a44efbc8b90
ariefhirmanto <ariefhirmanto7@gmail.com>
2021-08-24 22:51:54 +0700

    formatter response and slug

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,41 @@
 package campaign
+
+type CampaignFormatter struct {
+	ID               int    `json:"id"`
+	UserID           int    `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageURL         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+	Slug             string `json:"slug"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	formatter := CampaignFormatter{
+		ID:               campaign.ID,
+		Name:             campaign.Name,
+		UserID:           campaign.UserID,
+		ShortDescription: campaign.ShortDescription,
+		GoalAmount:       campaign.GoalAmount,
+		CurrentAmount:    campaign.CurrentAmount,
+		ImageURL:         "",
+		Slug:             campaign.Slug,
+	}
+
+	if len(campaign.CampaignImages) > 0 {
+		formatter.ImageURL = campaign.CampaignImages[0].FileName
+	}
+
+	return formatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	campaignsFormatter := []CampaignFormatter{}
+	for _, campaign := range campaigns {
+		campaignFormatter := FormatCampaign(campaign)
+		campaignsFormatter = append(campaignsFormatter, campaignFormatter)
+	}
+
+	return campaignsFormatter
+}
